91dcca2c4e04412d785c6a04dea31768e293f4bc
acl <acl447@nyu.edu>
2019-06-28 14:44:40 -0700

    Updates

---
+++ precache-manifest.a66f5ee6c0787231c2b09d1a5290fdfb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e92dc60474c0c2663a01f8bd881c575",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/clicky-game/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "eca3cc67bf8d66eb51d2",
+    "url": "/clicky-game/static/js/main.2dd60e55.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
