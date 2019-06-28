937b0604dcbe1d111d4c87307bb8709bacbb52bc
acl <acl447@nyu.edu>
2019-06-28 12:25:58 -0700

    styled navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,37 @@
 import React from 'react';
+import { blockStatement } from '@babel/types';
+
+
 
 
 
 function Navbar(props) {
 
-   
+    const styles = {
 
-  
-        return (<div className="navbar">
+        backgroundColor: '#5133a4',
+        color: 'white',
+        fontSize: '20px',
+        margin: 5,
+        padding: 5
 
-            <ul>
+    };
 
-                <li className="back-to-top">
-                    <a href="/">Clicky Game</a>
-                </li>
 
-                <li className="">
-                    Click an image to begin!
+
+
+    return (
+        <nav className="navbar navbar-expand-lg" style={styles}>
+            <ul className="navbar-nav mr-auto" style={styles}>
+            <li className="nav-item active" style={styles}>
+            <a href="/" style={styles}><strong>Clicky Game</strong></a>
             </li>
-
-                <li className="score">
-                    Score: {props.score} | Top Score: {props.topScore}
-                </li>
-
+            <li className="nav-item" style={styles}><div>Click an image to begin!</div></li>
+            <li className="nav-item" style={styles}> Score: {props.score} | Top Score: {props.topScore}</li>
             </ul>
-        </div>
-        );
-    }
+        </nav>
+    );
+}
 
 
 
